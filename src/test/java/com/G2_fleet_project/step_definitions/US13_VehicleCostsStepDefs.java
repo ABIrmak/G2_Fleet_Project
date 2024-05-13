dbab6b7a32adf9610a0e2fcd173755e0c53205a1
elifpehlivancelik <elifpehlivan1@gmail.com>
2024-05-13 23:21:01 +0300

    Vehicle cost feature file and step definitions added

---
+++ src/test/java/com/G2_fleet_project/step_definitions/US13_VehicleCostsStepDefs.java
@@ -0,0 +1,10 @@
+package com.G2_fleet_project.step_definitions;
+
+import com.G2_fleet_project.pages.VehicleCostsPage;
+
+public class US13_VehicleCostsStepDefs {
+
+    VehicleCostsPage vehicleCostsPage = new VehicleCostsPage();
+
+
+}
---
+++ src/test/resources/features/US13_VehicleCosts.feature
@@ -0,0 +1,6 @@
+Feature:As a user, (Store and Sales managers) I want to manage filters on the
+  Vehicle Costs page. (Web table and checkbox)
+
+  Background: Users should be able to login
+  When user is on the login page
+
