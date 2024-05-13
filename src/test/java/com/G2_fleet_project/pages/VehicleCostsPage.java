8344601a936d18a032b4f01d4a6c470e9a0289ab
elifpehlivancelik <elifpehlivan1@gmail.com>
2024-05-13 23:18:54 +0300

    Vehicle cost page added

---
+++ src/test/java/com/G2_fleet_project/pages/VehicleCostsPage.java
@@ -0,0 +1,20 @@
+package com.G2_fleet_project.pages;
+
+import com.G2_fleet_project.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class VehicleCostsPage {
+
+    public VehicleCostsPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "(//span[@class='grid-header-cell__label'])[1]")
+    public WebElement typeHeader;
+    @FindBy(xpath = "(//span[@class='grid-header-cell__label'])[2]")
+    public WebElement totalPriceHeader;
+    @FindBy(xpath = "(//span[@class='grid-header-cell__label'])[3]")
+    public WebElement dateHeader;
+}
